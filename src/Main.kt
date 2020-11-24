2b2a874cbcc73857ca664816e2befcc1687eb947
carolfons <carolinefonseca@gec.inatel.br>
2020-11-24 20:12:31 -0300

    Projeto Concluído

--- src/Main.kt
+++ src/Main.kt
@@ -1,46 +1,153 @@
 package org.kotlinlang.play
+
 fun main() {
 
     println("Bem vindo ao ByteBank")
-    println()
+    println("===========$$============")
 
-    val c1 = Conta()
-    val c2 = Conta()
+    val c1 = Conta("Alex", 1000)
+    val c2 = Conta("Fran", 1001)
 
     //conta 1
-    c1.titular = "Alex"
-    c1.numero = 1000
-    c1.saldo = 100.0
+    c1.deposita(200.0)
+
 
     //conta 2
-    c2.titular = "Fran"
-    c2.numero = 1001
-    c2.saldo = 200.0
+    c2.deposita(300.0)
 
     //saída de dados
     println("Conta de número ${c1.numero} pertence a ${c1.titular} possui o saldo de ${c1.saldo} reais")
     println("Conta de número ${c2.numero} pertence a ${c2.titular} possui o saldo de ${c2.saldo} reais")
+    println()
 
-   //depositando conta 1
-    deposita(c1, 50.0)
-    deposita(c2,70.0)
+    /*Testando as
+    Funções* */
+
+    var flag = true
+
+    while(flag) {
+
+
+        println("1 - DEPOSITAR")
+        println("2 - SACAR")
+        println("3 - TRANSFERIR")
+        println("4 - SAIR")
+
+        var inp = readLine()
+
+        if (inp == "1") {
+            c1.deposita(50.0)
+            c2.deposita(50.0)
+
+
+        } else if(inp == "2") {
+
+            c1.saca(100.0)
+            c2.saca(500.0)
+
+        }else if (inp == "3"){
+            if(c2.transfere(40.0,c1)){
+                println("Transferência efetuada com sucesso!")
+                println()
+            }
+            else{
+                println("Falha na transferência!")
+            }
+        }else{
+            flag = false
+        }
+    }
+
+//    //depositando conta 1
+//    c1.deposita(50.0)
+//    //depositando na conta 2
+//    c2.deposita(70.0)
+//
+//    //sacando da conta 1
+//    c1.saca(20.0)
+//
+//    //sacando da conta 2
+//    c2.saca(30.0)
+//
+//    //saque excedente
+//    c1.saca(500.0)
+//
+//    //transferencia conta 2 para conta 1
+//    if(c2.transfere(40.0, c1)){
+//        println("Transferência efetuada com sucesso!")
+//        println()
+//    }else{
+//        println("Falha na Transferência")
+//        println()
+//    }
 
 }
 
 
-}
+class Conta(var titular:String, val numero: Int) { //construtor primário, onde ja é possível declarar a variável
+     var saldo = 0.0
+        private set //setter privado apenas a própria classe altera seu valor diretamente
 
-
-class Conta{
-    var titular = ""
-    var numero = 0
-    var saldo = 0.0
-    
-    fun deposita(conta:Conta,valor:Double){
-        println("Depositando na conta do titular ${conta.titular} a quantia de $valor reais")
-        val valorAntigo = conta.saldo
-        conta.saldo+=valor
+    //método para depositar
+    fun deposita(valor: Double) {
+        println("Depositando na conta do titular ${this.titular} a quantia de $valor reais")
+        val valorAntigo = this.saldo
+        this.saldo += valor
         println("Saldo Inicial: $valorAntigo")
-        println("Saldo Final: ${conta.saldo} reais")
+        println("Saldo Final: ${this.saldo} reais")
+        println()
+    }
+
+
+    //método para sacar
+    fun saca(valor: Double) {
+        if (saldo >= valor) {
+            println("Sacando na conta do titular: ${this.titular}")
+            println("Quantidade Sacada: $valor")
+            val valorAntigo = this.saldo
+            this.saldo -= valor
+            println("Saldo Inicial: $valorAntigo")
+            println("Saldo Final: ${this.saldo}")
+            println()
+        } else {
+            println("Sacando na conta do titular: ${this.titular}")
+            println("Quantidade Sacada: $valor")
+            println("Erro! Valor de saque maior que saldo!")
+            println("Saldo da conta: ${this.saldo}")
+            println("Valor: $valor")
+            println()
+        }
+    }
+
+    //métoda para transferir
+    fun transfere(valor: Double, destino: Conta): Boolean {
+
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.deposita(valor)
+            println("Transferindo valor de $valor reais ")
+            println("Origem: Conta ${this.titular}")
+            println("Destino: Conta ${destino.titular}")
+            println()
+            return true
+        } else {
+            return false
+        }
+    }
+
+    /*
+      //getter e setters
+     fun getSaldo():Double{
+        return saldo
+    }
+    fun setSaldo(d: Double): Double {
+        return d
+    }
+    * */
+
+
+
+
+
 }
 
